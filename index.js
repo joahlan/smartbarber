2f6df22e4d952b4f81e8c4fc54f52e5830910bd8
DanDev <162236581+Danrp027@users.noreply.github.com>
2024-11-26 00:50:22 +0000

    boa noite

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-const express = require('express')
-const app = express()
-const sqlite = require('sqlite3')
+const express = require('express');
+const path = require('path');
+const sqlite = require('sqlite3').verbose();
+
+const app = express();
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const db = new sqlite.Database('SmartBarber.sqlite');
+
+function Clientes() {
+    var query = "CREATE TABLE IF NOT EXISTS CLIENTES("
+    query += 'ID INTEGER PRIMARY KEY AUTOINCREMENT,';
+    query += 'NOME VARCHAR(60),';
+    query += 'EMAIL VARCHAR(60),';
+    query += 'TELEFONE VARCHAR(11),';
+    query += 'SENHA VARCHAR(60)'
+    query += ');';
+}
+
+//Clientes()
+
+app.listen(7071, console.log('Rodando...'))
+
